e265db08a2210cd4920f5ae481f3b49df9b73b28
sadiwantoro <shadiwantoro@gmail.com>
2025-01-23 17:29:27 +0700

    first upload

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('pwa-cache').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/styles.css',
+        '/videos/iklan_stop_judi.mp4',
+        '/manifest.json',
+        '/icon-192x192.png',
+        '/icon-512x512.png',
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
